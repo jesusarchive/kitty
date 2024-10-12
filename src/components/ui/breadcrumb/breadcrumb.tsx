fcc9e140f43a88e60948910d25d501b6f5f5132d
jesusarchive <jbrdev21880949243098@protonmail.com>
2024-10-12 19:47:34 +0200

    chore: wip

--- src/components/ui/breadcrumb/breadcrumb.tsx
+++ src/components/ui/breadcrumb/breadcrumb.tsx
@@ -2,7 +2,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { ChevronRight, MoreHorizontal } from "lucide-react";
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils";
 
 export type BreadcrumbListProps = React.ComponentPropsWithoutRef<"ol">;
 export const BreadcrumbList = React.forwardRef<
--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils";
 
 export type CardHeaderProps = React.HTMLAttributes<HTMLDivElement>;
 export const CardHeader = React.forwardRef<HTMLDivElement, CardHeaderProps>(
